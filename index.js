6ac74ae5037ab0f91bbab56d0ff038ab8d37b684
Paul Selden <pselden@axs.com>
2014-08-07 09:31:51 -0400

    Adding dependency to test dependency resolution from github

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-exports.doSomethingFun = function(){
-    console.log("I'm doing something fun!");
+var async = require('async');
+
+exports.doSomethingFun = function doSomethingFun(msg){
+    msg = msg || '';
+    console.log("I'm doing something fun!" + msg);
 };
+
+exports.doSomethingFunInParallel = function(){
+    async.parallel([
+        somethingFunInParallel,
+        somethingFunInParallel,
+        somethingFunInParallel
+    ])
+
+    function somethingFunInParallel(callback){
+        setTimeout(function(){
+            doSomethingFun(" (In parallel) ");
+            callback();
+        }, 100);
+    }
+}
