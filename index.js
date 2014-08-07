149c75ec7b4446126109a6c89594a45b280db954
Paul Selden <pselden@axs.com>
2014-08-07 09:49:14 -0400

    Adding another dependency

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var async = require('async');
+var debug = require('debug')('common');
 
 exports.doSomethingFun = function(msg){
     msg = msg || '';
+    debug('doSomethingFun');
     console.log("I'm doing something fun!" + msg);
+
 };
 
 exports.doSomethingFunInParallel = function(){
+    debug('doSomethingFunInParallel');
     async.parallel([
         somethingFunInParallel,
         somethingFunInParallel,
